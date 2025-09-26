1547a5ca08dab279cc3dd20989cf418b06f01c5c
ishan199811 <ishansharma199811@gmail.com>
2025-09-26 14:09:40 +0530

    config

--- girrajmedico/src/main/java/com/girrajmedico/girrajmedico/configuration/WebSecurityConfig.java
+++ girrajmedico/src/main/java/com/girrajmedico/girrajmedico/configuration/WebSecurityConfig.java
@@ -66,6 +66,7 @@ public class WebSecurityConfig {
                     "/api/register",
                     "/api/docregister",
                     "/api/getAllMedicine",
+                    "/user/loggeduser",
                     "/**",
                     "/test"
                     
